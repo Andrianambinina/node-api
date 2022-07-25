719975ef6d71da8d0c0404ca2fa6a5494bce089b
Andrianambinina <zoandrianambinina@gmail.com>
2022-07-25 15:56:09 +0300

    Exposer l'api à tout le monde

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 require("./models/config");
 const postRoutes = require("./controllers/postsController");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use("/posts", postRoutes);
 
 app.listen(5500, () => console.log('Server started: 5500'));
